9220437dc4c7c7ac616a2cf0929a52061bd5eed1
Настя <zakharovanas0905@gmail.com>
2023-04-08 14:55:06 +0300

    ToyIterator

---
+++ Java/ToyIterator.java
@@ -0,0 +1,23 @@
+package Java;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ToyIterator<T> implements Iterator<T> {
+
+    public ToyIterator(List<T> toys) {
+    }
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+    }
+
+    @Override
+    public T next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+    
+}
