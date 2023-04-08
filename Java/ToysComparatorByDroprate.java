d447271ff9696e069e7a8ecd9c7ed726c40eaef9
Настя <zakharovanas0905@gmail.com>
2023-04-08 14:56:19 +0300

    ToysComparatorByDroprate

---
+++ Java/ToysComparatorByDroprate.java
@@ -0,0 +1,13 @@
+package Java;
+
+import java.util.Comparator;
+
+public class ToysComparatorByDroprate<T extends Toy> implements Comparator<T> {
+
+    @Override
+    public int compare(T o1, T o2) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+    }
+    
+}
